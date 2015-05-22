2c604a074ac2583cf796a84928b251f2a3fba9dc
dan <danbickford007@yahoo.com>
2015-05-21 22:15:50 -0400

    cleanup

--- compressor.rb
+++ compressor.rb
@@ -2,27 +2,25 @@ require 'zlib'
 
 class Compressor
 
+  def text 
+    path =  File.dirname(__FILE__)
+    @url = "#{path}/lib/dictionaries/english"
+    File.read(@url)
+  end
 
   def compress
-    path =  File.dirname(__FILE__)
-    url = "#{path}/lib/dictionaries/english"
-    text = File.read(url)
 
     compressed = Zlib::Deflate.deflate text
 
-    File.open(url, 'w') do |file|
+    File.open(@url, 'w') do |file|
       file.write compressed 
     end
   end
 
   def decompress
-    path =  File.dirname(__FILE__)
-    url = "#{path}/lib/dictionaries/english"
-    text = File.read(url)
-
     decompressed = Zlib::Inflate.inflate text
 
-    File.open(url, 'w') do |file|
+    File.open(@url, 'w') do |file|
       file.write decompressed 
     end
 
